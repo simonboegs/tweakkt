a3d1ef636c80535827a1d84d1738acb13f81c138
Simon Boeger <simonboegs@dhcp-39-23-184.dhcpint.csulb.edu>
2020-03-04 17:23:06 -0800

    Updates

---
+++ precache-manifest.39435b1260a3df44f57ed7c4f7f1dd62.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4007e50936a76c88b96d8b99138c4cf",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "1e3ac083d450d3e2198d",
+    "url": "/react-app/static/css/2.a87a9d52.chunk.css"
+  },
+  {
+    "revision": "cf4eadc674009327a607",
+    "url": "/react-app/static/css/main.ee4f0311.chunk.css"
+  },
+  {
+    "revision": "1e3ac083d450d3e2198d",
+    "url": "/react-app/static/js/2.c7433691.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.c7433691.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf4eadc674009327a607",
+    "url": "/react-app/static/js/main.ed7079dc.chunk.js"
+  },
+  {
+    "revision": "f981d4500eec1e2a5e52",
+    "url": "/react-app/static/js/runtime-main.4727a4a3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "399ac41c0a8bf2e571248e0b7f6824b3",
+    "url": "/react-app/static/media/meta-normal.399ac41c.ttf"
+  }
+]);
