5663df872fdb677523fab4546c688582e0ace54d
Simon Boeger <simonboegs@dhcp-39-23-184.dhcpint.csulb.edu>
2020-03-04 19:04:03 -0800

    Updates

---
+++ precache-manifest.b9a3af443a9265bc4690740e115df1d3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ab0958cdfac5acb9260625d73dd332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad49727ed83698a6af59",
+    "url": "/static/css/2.28dcdecb.chunk.css"
+  },
+  {
+    "revision": "0fb7a9956600d5e79346",
+    "url": "/static/css/main.b317c129.chunk.css"
+  },
+  {
+    "revision": "ad49727ed83698a6af59",
+    "url": "/static/js/2.c7433691.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7433691.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fb7a9956600d5e79346",
+    "url": "/static/js/main.693d7346.chunk.js"
+  },
+  {
+    "revision": "fbaf048f868100b869bd",
+    "url": "/static/js/runtime-main.8f658356.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "399ac41c0a8bf2e571248e0b7f6824b3",
+    "url": "/static/media/meta-normal.399ac41c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.39435b1260a3df44f57ed7c4f7f1dd62.js"
+  "/precache-manifest.b9a3af443a9265bc4690740e115df1d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
